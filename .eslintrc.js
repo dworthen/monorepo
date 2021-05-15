ad089d7f86fe93481f13f80077d792c674445420
Derek Worthen <worthend.derek@gmail.com>
2021-05-15 11:25:08 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['standard-with-typescript', 'eslint-config-prettier'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/indent': 'off',
+  },
+}
