c76a9ff2f49e8e3bab8d499b51d5af77acb0c050
Derek Worthen <worthend.derek@gmail.com>
2021-06-17 20:40:49 -0700

    update template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
+  plugins: ['simple-import-sort'],
   extends: ['standard-with-typescript', 'eslint-config-prettier'],
   parserOptions: {
     project: './tsconfig.json',
   },
   rules: {
+    'sort-imports': 'off',
+    'import/order': 'off',
+    'simple-import-sort/imports': 'error',
     '@typescript-eslint/space-before-function-paren': 'off',
     '@typescript-eslint/indent': 'off',
   },
